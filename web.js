7ed33cefdeab16c5a93dbc7b864a725fcb892a01
tranhaidang511 <tranhaidang511@gmail.com>
2013-07-04 19:18:08 +0900

    modified web.js to read index

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = fs.readFileSync("index.html");
+  var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
 });
 
